dd01642773a7a67bc7b499c2f98155d264505269
Matteo2104 <scarcella@outlook.it>
2022-03-28 15:35:04 +0200

    setup iniziale

---
+++ src/main/java/it/prova/gestionefascicoli/GestionefascicoliApplication.java
@@ -0,0 +1,13 @@
+package it.prova.gestionefascicoli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionefascicoliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionefascicoliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/prova/gestionefascicoli/GestionefascicoliApplicationTests.java
@@ -0,0 +1,13 @@
+package it.prova.gestionefascicoli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionefascicoliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
