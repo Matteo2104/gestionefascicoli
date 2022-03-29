c5dda9c12ec5d45dbcb95b8a4c0ffa3f98dd1319
flavio.g.amato <flavio.g.amato@gmail.com>
2022-03-29 10:11:01 +0200

    InsertFascicolo aggiunta

--- src/main/java/it/prova/gestionefascicoli/web/controller/FascicoloController.java
+++ src/main/java/it/prova/gestionefascicoli/web/controller/FascicoloController.java
@@ -1,11 +1,16 @@
 package it.prova.gestionefascicoli.web.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import it.prova.gestionefascicoli.dto.FascicoloDTO;
 import it.prova.gestionefascicoli.service.FascicoloService;
@@ -32,9 +37,28 @@ public class FascicoloController {
 
 	@PostMapping("/find")
 	public String find(FascicoloDTO example, Model model) {
-		model.addAttribute("list_fascicolo_attr",
-				FascicoloDTO.createFascicoloDTOListFromModelList(fascicoloService.findByExample(example.buildFascicoloModel(), null, null, null).toList()));
+		model.addAttribute("list_fascicolo_attr", FascicoloDTO.createFascicoloDTOListFromModelList(
+				fascicoloService.findByExample(example.buildFascicoloModel(), null, null, null).toList()));
 		return "fascicolo/list";
+	}
+
+	@GetMapping("/insert")
+	public String createFascicolo(Model model) {
+		model.addAttribute("insert_fascicolo_attr", new FascicoloDTO());
+		return "fascicolo/insert";
+	}
+
+	@PostMapping("/save")
+	public String saveRegista(@Valid @ModelAttribute("insert_fascicolo_attr") FascicoloDTO fascicoloDTO,
+			BindingResult result, RedirectAttributes redirectAttrs) {
+
+		if (result.hasErrors()) {
+			return "fascicolo/insert";
+		}
+		fascicoloService.inserisciFascicoloConDate(fascicoloDTO.buildFascicoloModel());
+
+		redirectAttrs.addFlashAttribute("successMessage", "Operazione eseguita correttamente");
+		return "redirect:/fascicolo";
 	}
 
 }
